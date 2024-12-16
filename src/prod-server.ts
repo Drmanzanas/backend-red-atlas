f7f0210808c047b6bd49f6e63dce14c96cd1614f
Ulises Menem <ulises@sidetool.co>
2024-12-16 01:47:22 -0300

    adding server for vercel

--- src/prod-server.ts
+++ src/prod-server.ts
@@ -2,13 +2,23 @@ import 'reflect-metadata';
 import { AppDataSource } from './database/data-source';
 import app from './app';
 
+let isDbConnected = false;
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Database connected!');
-  })
-  .catch((error) => {
-    console.error('Error during Data Source initialization', error);
-  });
+const initializeDb = async () => {
+  if (!isDbConnected) {
+    try {
+      await AppDataSource.initialize();
+      console.log('Database connected!');
+      isDbConnected = true;
+    } catch (error) {
+      console.error('Error during Data Source initialization', error);
+    }
+  }
+};
+
+app.use(async (req, res, next) => {
+  await initializeDb();
+  next();
+});
 
 export default app;
