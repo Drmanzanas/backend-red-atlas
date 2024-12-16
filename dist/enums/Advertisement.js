c1066cea25ce7682cebe30d482a7ad41e8e5c55c
Ulises Menem <ulises@sidetool.co>
2024-12-16 01:49:30 -0300

    borrando dist

--- dist/enums/Advertisement.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdvertisementStatus = void 0;
-var AdvertisementStatus;
-(function (AdvertisementStatus) {
-    AdvertisementStatus["FOR_SALE"] = "for_sale";
-    AdvertisementStatus["FOR_LEASE"] = "for_lease";
-})(AdvertisementStatus || (exports.AdvertisementStatus = AdvertisementStatus = {}));
